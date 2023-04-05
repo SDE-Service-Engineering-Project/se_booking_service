4083e4402e54a3b250e1c33a96fffd4706c2d44b
Sahil <sahil.singh98@hotmail.com>
2023-04-05 16:54:30 +0200

    initial commit

---
+++ src/main/java/at/ac/fhcampuswien/se_booking/SeBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package at.ac.fhcampuswien.se_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeBookingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/ac/fhcampuswien/se_booking/SeBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.fhcampuswien.se_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
