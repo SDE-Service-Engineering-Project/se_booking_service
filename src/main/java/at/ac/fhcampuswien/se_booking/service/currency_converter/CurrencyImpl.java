0a9156b2696b09fa76a491f7bbcf915ea2ad6c94
veidl <thomas.polzer@protonmail.com>
2023-04-22 21:36:49 +0200

    fix proto file

--- src/main/java/at/ac/fhcampuswien/se_booking/service/currency_converter/CurrencyImpl.java
+++ src/main/java/at/ac/fhcampuswien/se_booking/service/currency_converter/CurrencyImpl.java
@@ -1,7 +1,10 @@
 package at.ac.fhcampuswien.se_booking.service.currency_converter;
 
+import at.ac.fhcampuswien.se_booking.prototest.CurrencyService;
+import com.google.protobuf.Descriptors;
+
 public class CurrencyImpl  {
     public void test() {
-//        CurrencyService.getFromCurrency();
+        Descriptors.FileDescriptor descriptor = CurrencyService.getDescriptor();
     }
 }
